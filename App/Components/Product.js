7d0f567ff3de0b2b4c4806d3fc353aed47b6e0c5
Alex Zai <azai91@gmail.com>
2015-06-02 22:16:28 -0700

    (feat) remove sample data

--- App/Components/Product.js
+++ App/Components/Product.js
@@ -9,14 +9,6 @@ var {
   TouchableHighlight
 } = React;
 
-var sample = {
-  "name": "Streaks",
-  "tagline": "The to-do list for habits",
-  "screenshot_url": {
-    "850px": "https://api.url2png.com/v6/P5329C1FA0ECB6/5b750f810e43f6aa97e1e49cef4bf246/png/?thumbnail_max_width=850&url=http%3A%2F%2Fstreaksapp.com%2F"
-  }
-};
-
 class Product extends Component {
   render() {
     var product = this.props.product;
